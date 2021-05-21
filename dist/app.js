785e078ead3cdf0dd662adc15bf95ffb9bd7e3e1
yann <yanndubois4@gmail.com>
2021-05-21 17:07:48 +0200

    feat : add basic oop examples

--- dist/app.js
+++ dist/app.js
@@ -13,4 +13,61 @@ function clickHandler(message) {
 if (button) {
     button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
 }
+class Department {
+    constructor(id, name, employee = []) {
+        this.id = id;
+        this.name = name;
+        this.employee = employee;
+    }
+    describe() {
+        console.log(`Department: ${this.name} ID : ${this.id}`);
+    }
+    addEmployee(employee) {
+        this.employee.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employee.length);
+        console.log(this.employee);
+    }
+}
+const accounting = new Department("3", "Accounting");
+console.log(accounting.describe());
+accounting.addEmployee("Yann");
+accounting.addEmployee("Max");
+console.log(accounting.printEmployeeInformation());
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+class ITDepartment extends Department {
+    constructor(id, admin) {
+        super(id, "IT");
+        this.admin = admin;
+        this.admin = admin;
+    }
+}
+const IT = new ITDepartment("it", ["Hervé", "Samir"]);
+console.log(IT);
+class AccountingDept extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+    }
+    addEmployee(name) {
+        if (name === "Max") {
+            return;
+        }
+        this.employee.push(name);
+    }
+    addReports(report) {
+        this.reports.push(report);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const newAccounting = new AccountingDept("AC", []);
+console.log(newAccounting);
+newAccounting.addReports("Oups something went wrong! ");
+console.log(newAccounting.printReports());
+newAccounting.addEmployee("Max");
+newAccounting.addEmployee("Tony");
+newAccounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,83 @@ function clickHandler(message: string) {
 if (button) {
   button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
 }
+
+class Department {
+  //private name: string; // field of the class
+  // private employee: string[] = [];
+  constructor(
+    private readonly id: string,
+    public name: string,
+    protected employee: string[] = []
+  ) {
+    // this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name} ID : ${this.id}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employee.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employee.length);
+    console.log(this.employee);
+  }
+}
+
+const accounting = new Department("3", "Accounting");
+console.log(accounting.describe());
+accounting.addEmployee("Yann");
+accounting.addEmployee("Max");
+console.log(accounting.printEmployeeInformation());
+
+//const accountingCopy = { describe: accounting.describe }; // here we de-structure the object to assign a property that links to a Department method
+// accountingCopy.describe(); ts yield an error because the this: Department type made clear the describe method should be called with objects with a 'name' property
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe }; // ok
+//console.log(accountingCopy.describe());
+
+// inheritance of a class from another class with extends keyword
+class ITDepartment extends Department {
+  constructor(id: string, public admin: string[]) {
+    super(id, "IT");
+    this.admin = admin;
+  }
+}
+const IT = new ITDepartment("it", ["Hervé", "Samir"]);
+
+console.log(IT);
+
+class AccountingDept extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  // overriding a property inherited from the parent
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employee.push(name);
+  }
+
+  addReports(report: string) {
+    this.reports.push(report);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const newAccounting = new AccountingDept("AC", []);
+console.log(newAccounting);
+
+newAccounting.addReports("Oups something went wrong! ");
+console.log(newAccounting.printReports());
+newAccounting.addEmployee("Max");
+newAccounting.addEmployee("Tony");
+newAccounting.printEmployeeInformation(); // only one employee was added
