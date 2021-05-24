5177305b51f4c3a3cef351f3d466dc503befb338
yann <yanndubois4@gmail.com>
2021-05-24 13:54:33 +0200

    feat : add basic Classes examples

--- dist/app.js
+++ dist/app.js
@@ -19,8 +19,8 @@ class Department {
         this.name = name;
         this.employee = employee;
     }
-    describe() {
-        console.log(`Department: ${this.name} ID : ${this.id}`);
+    static createEmployee(name) {
+        return { name };
     }
     addEmployee(employee) {
         this.employee.push(employee);
@@ -30,17 +30,15 @@ class Department {
         console.log(this.employee);
     }
 }
-const accounting = new Department("3", "Accounting");
-console.log(accounting.describe());
-accounting.addEmployee("Yann");
-accounting.addEmployee("Max");
-console.log(accounting.printEmployeeInformation());
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+Department.fiscalYear = 2021;
 class ITDepartment extends Department {
     constructor(id, admin) {
         super(id, "IT");
         this.admin = admin;
         this.admin = admin;
+    }
+    describe() {
+        console.log(`IT department as id: ${this.id}`);
     }
 }
 const IT = new ITDepartment("it", ["Hervé", "Samir"]);
@@ -49,6 +47,28 @@ class AccountingDept extends Department {
     constructor(id, reports) {
         super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        else {
+            throw new Error("no report found.");
+        }
+    }
+    set mostRecentReport(text) {
+        if (!text) {
+            throw new Error("Please pass a valid value");
+        }
+        this.addReports(text);
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDept("CA", []);
+        return this.instance;
     }
     addEmployee(name) {
         if (name === "Max") {
@@ -56,17 +76,26 @@ class AccountingDept extends Department {
         }
         this.employee.push(name);
     }
+    describe() {
+        console.log(`the id is ${this.id}`);
+    }
     addReports(report) {
         this.reports.push(report);
+        this.lastReport = report;
     }
     printReports() {
         console.log(this.reports);
     }
 }
-const newAccounting = new AccountingDept("AC", []);
-console.log(newAccounting);
+const newAccounting = AccountingDept.getInstance();
+const newAccounting2 = AccountingDept.getInstance();
+console.log(newAccounting, "newAccounting");
+console.log(newAccounting2, "newAccounting2");
+console.log(newAccounting.describe(), "describe method on accounting instance");
 newAccounting.addReports("Oups something went wrong! ");
-console.log(newAccounting.printReports());
+newAccounting.mostRecentReport = "This is the last report";
+console.log(newAccounting.mostRecentReport, "mostRecentReport");
+console.log(newAccounting.printReports(), "printReports");
 newAccounting.addEmployee("Max");
 newAccounting.addEmployee("Tony");
 newAccounting.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -16,20 +16,22 @@ if (button) {
   button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
 }
 
-class Department {
+abstract class Department {
   //private name: string; // field of the class
   // private employee: string[] = [];
+  static fiscalYear = 2021;
   constructor(
-    private readonly id: string,
+    protected readonly id: string,
     public name: string,
-    protected employee: string[] = []
+    protected employee: string[] = [] // console.log(Department.fiscalYear) // static properties are not available with the this keyword that represents the instance of the class
   ) {
     // this.name = name;
   }
-
-  describe(this: Department) {
-    console.log(`Department: ${this.name} ID : ${this.id}`);
+  static createEmployee(name: string) {
+    return { name };
   }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employee.push(employee);
@@ -41,16 +43,20 @@ class Department {
   }
 }
 
-const accounting = new Department("3", "Accounting");
-console.log(accounting.describe());
-accounting.addEmployee("Yann");
-accounting.addEmployee("Max");
-console.log(accounting.printEmployeeInformation());
+// const accounting = new Department("3", "Accounting");
+// cannot create an instance of an abstract class
+// console.log(accounting.describe());
+// accounting.addEmployee("Yann");
+// accounting.addEmployee("Max");
+// console.log(accounting.printEmployeeInformation());
+
+// const employee1 = Department.createEmployee("Lenou");
+// console.log(employee1, Department.fiscalYear);
 
 //const accountingCopy = { describe: accounting.describe }; // here we de-structure the object to assign a property that links to a Department method
 // accountingCopy.describe(); ts yield an error because the this: Department type made clear the describe method should be called with objects with a 'name' property
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe }; // ok
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe }; // ok
 //console.log(accountingCopy.describe());
 
 // inheritance of a class from another class with extends keyword
@@ -59,18 +65,47 @@ class ITDepartment extends Department {
     super(id, "IT");
     this.admin = admin;
   }
+  describe() {
+    console.log(`IT department as id: ${this.id}`);
+  }
 }
 const IT = new ITDepartment("it", ["Hervé", "Samir"]);
 
 console.log(IT);
 
 class AccountingDept extends Department {
-  constructor(id: string, private reports: string[]) {
+  private lastReport: string;
+  private static instance: AccountingDept;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    } else {
+      throw new Error("no report found.");
+    }
+  }
+
+  set mostRecentReport(text: string) {
+    if (!text) {
+      throw new Error("Please pass a valid value");
+    }
+    this.addReports(text);
+  }
+
+  private constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDept("CA", []);
+    return this.instance;
   }
 
   // overriding a property inherited from the parent
-
   addEmployee(name: string) {
     if (name === "Max") {
       return;
@@ -78,8 +113,13 @@ class AccountingDept extends Department {
     this.employee.push(name);
   }
 
+  describe() {
+    console.log(`the id is ${this.id}`);
+  }
+
   addReports(report: string) {
     this.reports.push(report);
+    this.lastReport = report;
   }
 
   printReports() {
@@ -87,11 +127,21 @@ class AccountingDept extends Department {
   }
 }
 
-const newAccounting = new AccountingDept("AC", []);
-console.log(newAccounting);
+// const newAccounting = new AccountingDept("CA", []);
+const newAccounting = AccountingDept.getInstance();
+const newAccounting2 = AccountingDept.getInstance();
+
+console.log(newAccounting, "newAccounting");
+console.log(newAccounting2, "newAccounting2"); // exact same object instantiated
+
+console.log(newAccounting.describe(), "describe method on accounting instance");
 
 newAccounting.addReports("Oups something went wrong! ");
-console.log(newAccounting.printReports());
+newAccounting.mostRecentReport = "This is the last report";
+
+console.log(newAccounting.mostRecentReport, "mostRecentReport");
+
+console.log(newAccounting.printReports(), "printReports");
 newAccounting.addEmployee("Max");
 newAccounting.addEmployee("Tony");
 newAccounting.printEmployeeInformation(); // only one employee was added
